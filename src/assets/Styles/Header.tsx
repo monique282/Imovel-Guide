6c8e14cf976c4799e93a06b372fd6afbfb1170e3
Monique de Souza <moniquejp16@gmail.com>
2024-10-10 17:51:10 -0300

    feat: componentizing the style

---
+++ src/assets/Styles/Header.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+   width: 100%;
+   height: 60px;
+   //background-color: #1B2653;
+   background-color: #1B26;
+   display: flex; 
+   align-items: center; 
+   //justify-content: center; 
+   img {
+    
+   }
+`;
--- src/component/‎Header.tsx
+++ src/component/‎Header.tsx
@@ -1,5 +1,5 @@
-import slogan from "../assets/images/slogan.png";
-import styled from "styled-components";
+import { HeaderContainer } from '../assets/Styles/Header';
+import slogan from '../assets/images/slogan.png';
 
 
 export default function Header() {
@@ -10,16 +10,4 @@ export default function Header() {
     );
 }
 
-const HeaderContainer = styled.div`
-   width: 100%;
-   height: 60px;
-   background-color: #1B2653;
-   align-items: center; /* Centraliza o conteúdo verticalmente */
-   padding: 0; /* Remove qualquer padding que possa estar aplicado */
-   margin:-10; /* Remove qualquer margem que possa estar aplicada */
-   
-   img {
-       margin: 0; /* Remove a margem padrão da imagem */
-       padding: 0; /* Remove o padding da imagem */
-   }
-`;
+;
