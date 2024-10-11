9422fd3d8e965855a2a8cfe17f809ab102b418e1
Monique de Souza <moniquejp16@gmail.com>
2024-10-11 12:41:37 -0300

    feat: cleaning the field after sending

--- src/component/Message.tsx
+++ src/component/Message.tsx
@@ -1,15 +1,45 @@
+import { useState } from "react";
 import { Message } from "../assets/Styles/Home";
 import InputMask from "react-input-mask";
 
 export default function MessageHome() {
 
+    const [cpf, setCpf] = useState(""); 
+    const [phone, setPhone] = useState(""); 
+    const [subject, setSubject] = useState(""); 
+
+    const sendData= () => {
+        setCpf(""); 
+        setPhone(""); 
+        setSubject(""); 
+    };
+
     return (
         <Message>
             <h1 className="title-message">Mande uma mensagem:</h1>
-            <InputMask mask="999.999.999-99" className="cpf-message" placeholder="Digite seu CPF" />
-            <InputMask mask="(99) 99999-9999" className="phone-message" placeholder="Digite seu telefone" />
-            <textarea className="subject-message" placeholder="Digite o assunto" maxLength={200}></textarea>
-            <button className="toSend-message">Enviar Mensagem</button>
+            <InputMask 
+                mask="999.999.999-99" 
+                className="cpf-message" 
+                placeholder="Digite seu CPF" 
+                value={cpf} 
+                onChange={(e) => setCpf(e.target.value)} 
+            />
+            <InputMask 
+                mask="(99) 99999-9999" 
+                className="phone-message" 
+                placeholder="Digite seu telefone" 
+                value={phone} 
+                onChange={(e) => setPhone(e.target.value)} 
+            />
+            <textarea 
+                className="subject-message" 
+                placeholder="Digite o assunto" 
+                maxLength={200} 
+                value={subject} 
+                onChange={(e) => setSubject(e.target.value)} 
+            ></textarea>
+            <button className="toSend-message" onClick={sendData}>Enviar Mensagem</button> 
+        
         </Message>
 
     );
