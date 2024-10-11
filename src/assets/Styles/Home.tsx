3279e84de592fc7f12395b2fd85a4d4856e1682f
Monique de Souza <moniquejp16@gmail.com>
2024-10-10 22:37:31 -0300

    feat: creation of the first three boxes

--- src/assets/Styles/Home.tsx
+++ src/assets/Styles/Home.tsx
@@ -1,2 +1,55 @@
 import styled from "styled-components";
 
+export const OnPop = styled.div`
+    width: 100%;
+    background-color: red;
+    display: flex; 
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 50px;
+`;
+
+export const BrokersProfile = styled.div`
+    width: 200px;
+    margin-right : 50px ;
+`;
+
+export const Broker = styled.div`
+    height: 341px;
+    width: 379px;
+    border-radius: 10px;
+    border: 2px solid black;
+    background-color: #0015ff;
+
+img{
+    width: 200px;
+
+}
+`;
+
+
+export const Data = styled.div`
+    display: flex;
+`;
+
+export const Message = styled.div`
+    height: 341px;
+    width: 379px;
+    border-radius: 10px;
+    border: 2px solid black;
+    background-color: #15ff00;
+
+`;
+
+export const Rule3 = styled.div`
+    height: 341px;
+    width: 379px;
+    border-radius: 10px;
+    border: 2px solid black;
+`;
+
+export const Below = styled.div`
+    width: 100%;
+    background-color: blue;
+`;
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,57 @@
+import { OnPop, BrokersProfile, Message, Rule3, Below, Broker, Data } from "../assets/Styles/Home";
+import html2canvas from "html2canvas";
+import { useRef } from "react";
 
 export default function Home() {
- 
+  const listBroker = [
+    { image: "https://img.freepik.com/fotos-gratis/mulher-de-negocios-elegante-e-confiante-sorrindo_176420-19466.jpg", name: "Monique", points: "1200", telephone: "38998152601" },
+    { image: "https://img.freepik.com/fotos-gratis/homem-bonito-e-confiante-sorrindo-com-as-maos-cruzadas-no-peito_176420-18743.jpg", name: "Patrick", points: "1100", telephone: "34997734653" }
+  ];
+
+  // Referência para o container dos corretores que será capturado
+  const brokersRef = useRef(null);
+
+  // Função para capturar e baixar a imagem
+  const downloadImage = () => {
+    if (brokersRef.current) {
+      html2canvas(brokersRef.current).then(canvas => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = 'brokers.png';
+        link.click();
+      });
+    }
+  };
+
   return (
-    <> ola mundo
+    <>
+      <OnPop>
+        <BrokersProfile ref={brokersRef}>
+          <Broker >
+            {listBroker.map((broker, index) => (
+              <Data key={index}>
+                <>    
+                <img src={broker.image} alt="Broker profile" />
+                </>
+                <>
+                <h2>{broker.name}</h2>
+                <p>Points: {broker.points}</p>
+                <p>Telephone: {broker.telephone}</p>
+                </>
+              </Data>
+            ))}
+          </Broker>
+        </BrokersProfile>
+        <Message>
+
+        </Message>
+        <Rule3>
+
+        </Rule3>
+      </OnPop>
+      <Below>
+
+      </Below>
     </>
   );
 }
