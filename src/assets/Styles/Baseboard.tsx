ca8a9a695ccef1ee7359445de7ba4bdc107588fc
Monique de Souza <moniquejp16@gmail.com>
2024-10-11 21:03:04 -0300

    feat:modal image

--- src/assets/Styles/Baseboard.tsx
+++ src/assets/Styles/Baseboard.tsx
@@ -20,3 +20,49 @@ export const RightSide = styled.div`
         margin-right: 30px;
     }
 `;
+
+export const ModalStyles = styled.div`
+    position: fixed;
+    z-index: 2;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+  export const ModalContentStyles = styled.div`
+    width: 50%;
+    height: 50%;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 10px;
+    position: relative;
+    text-align: center;
+`;
+  export const CloseButtonStyles = styled.span`
+    width: 100%;
+    position: absolute;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    color: #FE6613;
+    font-family: Roboto;
+    top: 10px;
+    right: 0px;
+    padding-right: 20px;
+    box-sizing: border-box;
+    border-bottom: 1px solid black;
+    font-size: 30px;
+    cursor: pointer;
+`;
+  export const ModalImageStyles = styled.img`
+    max-width: 100%;  
+    max-height: 95%;
+    margin-top: 30px;
+    background-color: #153ee1;
+`;
+  
+  
--- src/component/Home/ModalImage.tsx
+++ src/component/Home/ModalImage.tsx
@@ -1,17 +1,38 @@
+import React, { useState } from "react";
 import { HomeImage, Image } from "../../assets/Styles/Home";
-
+import { CloseButtonStyles, ModalContentStyles, ModalImageStyles, ModalStyles } from "../../assets/Styles/Baseboard";
 
 export default function ModalImage() {
-  const listImage = "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_640.jpg"
+  const [showModal, setShowModal] = useState(false);
+  const listImage = "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_640.jpg";
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <HomeImage>
-      <Image >
-        <img className="home-image" src={listImage} />
+      <Image>
+        <img className="home-image" src={listImage} alt="Casa" />
       </Image>
-      <button className="buton-image"
-      >
+      <button className="buton-image" onClick={handleOpenModal}>
         ABRIR IMAGEM NO MODAL
       </button>
+      {showModal && (
+        <ModalStyles className="modal">
+          <ModalContentStyles className="modal-content" >
+            <CloseButtonStyles className="close" onClick={handleCloseModal}>
+              x
+            </CloseButtonStyles>
+            <ModalImageStyles src={listImage} alt="Imagem no Modal" />
+          </ModalContentStyles>
+        </ModalStyles>
+      )}
     </HomeImage>
   );
 }
+
