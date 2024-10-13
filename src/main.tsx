eeadaec888c923700bb7c9e1c1a0d3012dfb8979
Monique de Souza <moniquejp16@gmail.com>
2024-10-13 13:47:26 -0300

    feat: add logo

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import Reset from './assets/Styles/Reset.js'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-     <Reset />
+    <Reset />
     <App />
   </React.StrictMode>,
 )
